f7e45dd08042b34faba270659b72720b4a78be29
Sale19 <sale1981@hotmail.com>
2024-08-19 21:59:15 +0200

    About page fixed

--- api/routes/about/about.php
+++ api/routes/about/about.php
@@ -8,9 +8,7 @@ function handleFileUpload()
     $data = array();
 
     if (isset($_FILES['upload']['name'])) {
-
         $filename = $_FILES['upload']['name'];
-
         $uploadDirectory = __DIR__ . '/../../../storage/about/';
         $location = $uploadDirectory . $filename;
 
@@ -43,22 +41,21 @@ function handleFileUpload()
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
     if (isset($_POST['title']) && isset($_POST['content'])) {
         $title = $_POST['title'];
         $content = $_POST['content'];
 
         if ($title && $content) {
-            $stmt = $pdo->prepare("SELECT id, title, content FROM about LIMIT 1");
+            $stmt = $pdo->prepare("SELECT id FROM about LIMIT 1");
             $stmt->execute();
             $existingRecord = $stmt->fetch(PDO::FETCH_ASSOC);
 
             if ($existingRecord) {
-                $stmt = $pdo->prepare("UPDATE about SET title = :title, content = :content WHERE id = :id");
+                $stmt = $pdo->prepare("UPDATE about SET title = :title, content = :content, last_updated = NOW() WHERE id = :id");
                 $stmt->execute([':title' => $title, ':content' => $content, ':id' => $existingRecord['id']]);
                 echo json_encode(['success' => true, 'message' => 'Record updated successfully.']);
             } else {
-                $stmt = $pdo->prepare("INSERT INTO about (title, content) VALUES (:title, :content)");
+                $stmt = $pdo->prepare("INSERT INTO about (title, content, last_updated) VALUES (:title, :content, NOW())");
                 $stmt->execute([':title' => $title, ':content' => $content]);
                 echo json_encode(['success' => true, 'message' => 'Record created successfully.']);
             }
@@ -70,7 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode($uploadData);
     }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $stmt = $pdo->prepare("SELECT title, content FROM about ORDER BY created_at DESC LIMIT 1");
+    $stmt = $pdo->prepare("SELECT title, content, DATE_FORMAT(last_updated, '%W, %e %M %Y') as last_updated FROM about ORDER BY last_updated DESC LIMIT 1");
     $stmt->execute();
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
